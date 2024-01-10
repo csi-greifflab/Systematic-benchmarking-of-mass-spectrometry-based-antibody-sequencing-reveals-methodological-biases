ab34352340fcba7164f6776a32c8d1527294d5c5
Khang Le Quy <88042915+Khang-LQ@users.noreply.github.com>
2024-01-10 12:09:48 +0100

    Code for supp figures

---
+++ Khang_config.R
@@ -0,0 +1,36 @@
+library(Rcpp)
+library(ggplot2)
+library(readr)
+library(stringr)
+library(reshape2) 
+library(readxl)
+library(rio)
+library(seqinr)
+library(ComplexHeatmap)
+library(dplyr)
+library(RColorBrewer)
+library(forcats)
+#library(DescTools)
+library(circlize)
+#library(googledrive)
+
+project_path <- "/Users/khangl/UiO Dropbox/Khang Le Quy/CSI/datasets/csi_datasets/ms_benchmarking/msms_figures"
+
+source(file.path(project_path, "data/preprocessing_functions.R"))
+source(file.path(project_path, "plots.R"))
+source(file.path(project_path, "utils.R"))
+
+enzyme_cols <- c("All" = "#575A6C",
+                 "AspN" = "#B4C540",
+                 "Tryp" = "#E84D8A",
+                 "Ct" = "#64C5EB",
+                 "Ct+Tryp" = "#7F58AF")
+
+
+
+metadata_path <- file.path(project_path, "metadata")
+data_path <- file.path(project_path, "data")
+
+fig2_path <- file.path(project_path, "fig2")
+fig3_path <- file.path(project_path, "fig2")
+fig5_path <- file.path(project_path, "fig5")
